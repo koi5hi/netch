5cd7847a43c266f49c9ec2b7239f6e2702652071
koi5hi <50271567+koi5hi@users.noreply.github.com>
2023-07-31 07:27:19 +0800

    Update Socks5Controller.cs
    修复socks5 udp

--- Netch/Servers/Socks5/Socks5Controller.cs
+++ Netch/Servers/Socks5/Socks5Controller.cs
@@ -8,9 +8,9 @@ public class Socks5Controller : V2rayController
 
     public override Task<Socks5Server> StartAsync(Server s)
     {
-        var server = (Socks5Server)s;
-        if (!server.Auth())
-            throw new ArgumentException();
+        // var server = (Socks5Server)s;
+        // if (!server.Auth())
+        //     throw new ArgumentException();
 
         return base.StartAsync(s);
     }
